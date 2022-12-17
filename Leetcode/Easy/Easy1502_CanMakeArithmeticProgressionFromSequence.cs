48cb0291d92d44353003affa274cf3cc9efcbe54
Komroniddin Soliev <kamron.saliev5@gmail.com>
2022-12-18 00:57:52 +0500

    Added solution for 1502. Can Make Arithmetic Progression From Sequence

---
+++ Leetcode/Easy/Easy1502_CanMakeArithmeticProgressionFromSequence.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+    /// </summary>
+    public class Easy1502_CanMakeArithmeticProgressionFromSequence
+    {
+        public bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+
+            var difference = arr[1] - arr[0];
+
+            for (var i = 2; i < arr.Length; i++)
+            {
+                var temp = arr[i] - arr[i - 1];
+
+                if (difference != temp)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
