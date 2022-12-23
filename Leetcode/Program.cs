d6476174c50741e286bd7f4960292eb307c703d0
Komroniddin Soliev <kamron.saliev5@gmail.com>
2022-12-24 00:48:01 +0500

    Added sample code used in the previous contests

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -8,9 +8,42 @@ namespace Leetcode
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
+            var t = 1; // by default
+            // t = int.Parse(Console.ReadLine()!);
             
+            for (int i = 0; i < t; i++)
+            {
+                Solve();
+            }
+        }
+        
+        private static void Solve()
+        {
+            var n = int.Parse(Console.ReadLine()!);
+ 
+            var ans = 0;
+            var sum = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                var nums = Array.ConvertAll(Console.ReadLine()!.Trim().Split(' '), Convert.ToInt32);
+ 
+                sum = 0;
+                
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    sum += nums[j];
+                }
+ 
+                if (sum >= 2)
+                {
+                    ans++;
+                }
+            }
+ 
+            Console.WriteLine(ans);
         }
     }
 }
---
+++ Leetcode/Temp.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Leetcode
+{
+    public class Temp
+    {
+        /*static void Main(string[] args)
+        {
+            var t = 1; // by default
+            // t = int.Parse(Console.ReadLine()!);
+            
+            for (int i = 0; i < t; i++)
+            {
+                Solve();
+            }
+        }
+        
+        private static void Solve()
+        {
+            var n = int.Parse(Console.ReadLine()!);
+            var nums = Array.ConvertAll(Console.ReadLine()!.Trim().Split(' '), Convert.ToInt32);
+        }*/
+    }
+}
