2656b426c0094163eed0fa263da1df0eba1aa22d
Komroniddin Soliev <kamron.saliev5@gmail.com>
2022-12-27 20:23:20 +0500

    Added solution for 125. Valid Palindrome

---
+++ Leetcode/Easy/Easy125_ValidPalindrome.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/valid-palindrome/
+    /// </summary>
+    public class Easy125_ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+            var startIndex = 0;
+            var endIndex = s.Length - 1;
+
+            while (startIndex < endIndex)
+            {
+                if (!char.IsLetterOrDigit(s[startIndex]))
+                {
+                    startIndex++;
+                    continue;
+                }
+
+                if (!char.IsLetterOrDigit(s[endIndex]))
+                {
+                    endIndex--;
+                    continue;
+                }
+
+                if (s[startIndex++] != s[endIndex--])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
