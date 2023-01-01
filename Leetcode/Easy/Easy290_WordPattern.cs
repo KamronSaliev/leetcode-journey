fec8bdc6625fd9410dd5960b73d6ed5c8144f7bd
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-01 23:30:07 +0500

    Added solution for 290. Word Pattern

---
+++ Leetcode/Easy/Easy290_WordPattern.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/word-pattern/
+    /// </summary>
+    public class Easy290_WordPattern
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            var words = s.Split(' ');
+            var dict1 = new Dictionary<char, string>();
+            var dict2 = new Dictionary<string, char>();
+
+            if (words.Length != pattern.Length)
+            {
+                return false;
+            }
+            
+            for (var i = 0; i < words.Length; i++)
+            {
+                dict1.TryAdd(pattern[i], words[i]);
+                dict2.TryAdd(words[i], pattern[i]);
+
+                if (dict1[pattern[i]] != words[i] || dict2[words[i]] != pattern[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
