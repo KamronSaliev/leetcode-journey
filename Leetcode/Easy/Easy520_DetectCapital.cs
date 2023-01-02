a5f2d33e3a64452dc038b4a82130591f2ec22c51
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-02 13:27:32 +0500

    Added solution for 520. Detect Capital

---
+++ Leetcode/Easy/Easy520_DetectCapital.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/detect-capital/
+    /// </summary>
+    public class Easy520_DetectCapital
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            var capitalCounter = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsUpper(word[i]))
+                {
+                    capitalCounter++;
+                }
+            }
+
+            return capitalCounter == 0 || capitalCounter == word.Length ||
+                   (capitalCounter == 1 && char.IsUpper(word[0]));
+        }
+    }
+}
