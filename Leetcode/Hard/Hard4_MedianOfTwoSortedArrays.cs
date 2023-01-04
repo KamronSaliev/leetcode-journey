ec6f6f3fa72b1a4280e1990f3b9c1778223d707e
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-04 20:55:05 +0500

    Added solution for 4. Median of Two Sorted Arrays

---
+++ Leetcode/Hard/Hard4_MedianOfTwoSortedArrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Leetcode.Hard
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/median-of-two-sorted-arrays/
+    /// </summary>
+    public class Hard4_MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var combined = nums1.Concat(nums2).ToArray();
+
+            Array.Sort(combined);
+
+            var mid = combined.Length / 2;
+
+            if (combined.Length % 2 == 0)
+            {
+                return (combined[mid - 1] + combined[mid]) / 2.0d;
+            }
+
+            return combined[mid];
+        }
+    }
+}
