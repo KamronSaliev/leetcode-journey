2c1025b74301b82f2dda10ae2b75b3f17edfe345
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-09 14:17:02 +0500

    Added solution for 145. Binary Tree Postorder Traversal

---
+++ Leetcode/Easy/Easy145_BinaryTreePostorderTraversal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Leetcode.Utilities;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/binary-tree-postorder-traversal/
+    /// </summary>
+    public class Easy145_BinaryTreePostorderTraversal
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            return PostorderTraversal(root, result);
+        }
+
+        private IList<int> PostorderTraversal(TreeNode current, List<int> result)
+        {
+            if (current == null)
+            {
+                return result;
+            }
+
+            PostorderTraversal(current.left, result);
+            PostorderTraversal(current.right, result);
+            result.Add(current.val);
+
+            return result;
+        }
+    }
+}
