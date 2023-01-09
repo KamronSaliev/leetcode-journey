33411c054b75cf58738de6840a15a2197a0e83f6
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-09 14:16:45 +0500

    Added solution for 144. Binary Tree Preorder Traversal

---
+++ Leetcode/Easy/Easy144_BinaryTreePreorderTraversal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Leetcode.Utilities;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/binary-tree-preorder-traversal/
+    /// </summary>
+    public class Easy144_BinaryTreePreorderTraversal
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            return PreorderTraversal(root, result);
+        }
+
+        private IList<int> PreorderTraversal(TreeNode current, IList<int> result)
+        {
+            if (current == null)
+            {
+                return result;
+            }
+
+            result.Add(current.val);
+            PreorderTraversal(current.left, result);
+            PreorderTraversal(current.right, result);
+
+            return result;
+        }
+    }
+}
