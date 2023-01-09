e760e381cdd8a0845fc3c6b3d3355c3c9cf1de89
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-09 14:16:24 +0500

    Added solution for 94. Binary Tree Inorder Traversal

---
+++ Leetcode/Easy/Easy94_BinaryTreeInorderTraversal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Leetcode.Utilities;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/binary-tree-inorder-traversal/
+    /// </summary>
+    public class Easy94_BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            return InorderTraversal(root, result);
+        }
+
+        private IList<int> InorderTraversal(TreeNode current, IList<int> result)
+        {
+            if (current == null)
+            {
+                return result;
+            }
+
+            InorderTraversal(current.left, result);
+            result.Add(current.val);
+            InorderTraversal(current.right, result);
+
+            return result;
+        }
+    }
+}
