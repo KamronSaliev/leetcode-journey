15531e6623ca4d2f39e4aef592177f956274642a
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-11 19:35:10 +0500

    Added solution for 104. Maximum Depth of Binary Tree

---
+++ Leetcode/Easy/Easy104_MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Leetcode.Utilities;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    /// </summary>
+    public class Easy104_MaximumDepthOfBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var leftMaxDepth = MaxDepth(root.left);
+            var rightMaxDepth = MaxDepth(root.right);
+
+            return Math.Max(leftMaxDepth, rightMaxDepth) + 1;
+        }
+    }
+}
