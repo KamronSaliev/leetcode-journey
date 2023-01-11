fcd882893a7abeb13d6a515549327214abe9e7f7
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-11 19:34:38 +0500

    Added solution for 326. Power of Three

---
+++ Leetcode/Easy/Easy326_PowerOfThree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/power-of-three/
+    /// </summary>
+    public class Easy326_PowerOfThree
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            return Math.Log10(n) / Math.Log10(3) % 1 == 0;
+        }
+    }
+}
