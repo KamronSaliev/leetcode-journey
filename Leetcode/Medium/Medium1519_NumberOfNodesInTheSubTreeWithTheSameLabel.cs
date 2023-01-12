d5652ddba3ca9978cfe1c4a2883d0b2bd9b28e1e
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-12 23:52:48 +0500

    Added solution for 1519. Number of Nodes in the Sub-Tree With the Same Label

---
+++ Leetcode/Medium/Medium1519_NumberOfNodesInTheSubTreeWithTheSameLabel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Leetcode.Utilities;
+
+namespace Leetcode.Medium
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/
+    /// </summary>
+    public class Medium1519_NumberOfNodesInTheSubTreeWithTheSameLabel
+    {
+        public int[] CountSubTrees(int n, int[][] edges, string labels)
+        {
+            var graph = GraphUtilities.BuildGraph(edges);
+
+            var result = new int[n];
+            DFS(0, -1, labels, result, graph);
+
+            return result;
+        }
+
+        private int[] DFS(int node, int parent, string labels, int[] result, Dictionary<int, List<int>> graph)
+        {
+            var cnts = new int[26];
+            cnts[labels[node] - 'a']++;
+
+            foreach (var child in graph[node])
+            {
+                if (child == parent)
+                {
+                    continue;
+                }
+
+                var childCount = DFS(child, node, labels, result, graph);
+
+                for (var i = 0; i < 26; i++)
+                {
+                    cnts[i] += childCount[i];
+                }
+            }
+
+            result[node] = cnts[labels[node] - 'a'];
+
+            return cnts;
+        }
+    }
+}
