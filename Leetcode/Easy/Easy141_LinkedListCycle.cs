4654ea67281cc129add431759174f29d3ea09b5c
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-25 14:33:33 +0500

    Added solution for 141. Linked List Cycle

---
+++ Leetcode/Easy/Easy141_LinkedListCycle.cs
@@ -0,0 +1,29 @@
+﻿using Leetcode.Utilities;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/linked-list-cycle/
+    /// </summary>
+    public class Easy141_LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
