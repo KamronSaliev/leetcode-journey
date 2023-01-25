7e0d75a82797b0e47ffc50d18215ddb798d210ef
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-26 00:23:48 +0500

    Added solution for 349. Intersection of Two Arrays

---
+++ Leetcode/Easy/Easy349_IntersectionOfTwoArrays.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/intersection-of-two-arrays/
+    /// </summary>
+    public class Easy349_IntersectionOfTwoArrays
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var intersection = new HashSet<int>();
+
+            for (var i = 0; i < nums2.Length; i++)
+            {
+                if (nums1.Contains(nums2[i]))
+                {
+                    intersection.Add(nums2[i]);
+                }
+            }
+
+            return intersection.ToArray();
+        }
+    }
+}
