7e8b00d27f8bd63f1a91fef5954f89742686128e
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-27 01:24:18 +0500

    Added solution for 169. Majority Element

---
+++ Leetcode/Easy/Easy169_MajorityElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/majority-element/
+    /// </summary>
+    public class Easy169_MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var occurrences = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (occurrences.ContainsKey(nums[i]))
+                {
+                    occurrences[nums[i]]++;
+                }
+                else
+                {
+                    occurrences.Add(nums[i], 1);
+                }
+
+                if (occurrences[nums[i]] > nums.Length / 2)
+                {
+                    return nums[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
