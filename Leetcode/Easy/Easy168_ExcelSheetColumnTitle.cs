ca4f21a272d37d7f3a5c94aaf816a8059be77adb
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-28 00:50:34 +0500

    Added solution for 168. Excel Sheet Column Title

---
+++ Leetcode/Easy/Easy168_ExcelSheetColumnTitle.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/excel-sheet-column-title/
+    /// </summary>
+    public class Easy168_ExcelSheetColumnTitle
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            var result = string.Empty;
+
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                result = (char)(columnNumber % 26 + 'A') + result;
+                columnNumber /= 26;
+            }
+
+            return result;
+        }
+    }
+}
