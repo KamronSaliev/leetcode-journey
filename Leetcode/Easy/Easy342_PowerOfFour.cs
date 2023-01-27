b30e17e03d531892f83d3a167ed2a47209b23606
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-28 00:52:34 +0500

    Added solution for 342. Power of Four

---
+++ Leetcode/Easy/Easy342_PowerOfFour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Leetcode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/power-of-four/
+    /// </summary>
+    public class Easy342_PowerOfFour
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            return Math.Log10(n) / Math.Log10(4) % 1 == 0;
+        }
+    }
+}
