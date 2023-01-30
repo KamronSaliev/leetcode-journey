6a81b5fd797460aa5ace6ff003e9e36e91c73a82
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-01-30 22:59:29 +0500

    Added solution for 328. Odd Even Linked List

---
+++ Leetcode/Medium/Medium328_OddEvenLinkedList.cs
@@ -0,0 +1,29 @@
+﻿using Leetcode.Utilities;
+
+namespace Leetcode.Medium
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/odd-even-linked-list/
+    /// </summary>
+    public class Medium328_OddEvenLinkedList
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            var odd = head;
+            var even = head.next;
+            var evenStart = head.next;
+
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+
+            odd.next = evenStart;
+
+            return head;
+        }
+    }
+}
