b0ccfd7d9fc41f5cfea887a0f3021b9c2e097cc7
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-02-08 16:38:49 +0500

    Updated solution for 3. Longest Substring Without Repeating Characters

--- leetcode/Medium/Medium3_LongestSubstringWithoutRepeatingCharacters.cs
+++ leetcode/Medium/Medium3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -9,21 +9,18 @@ namespace LeetCode.Medium
     {
         public int LengthOfLongestSubstring(string s)
         {
-            var notDuplicates = new HashSet<char>();
+            var characters = new HashSet<char>();
             var tempString = new Queue<char>();
             var maxLength = 0;
 
             for (var i = 0; i < s.Length; i++)
             {
-                if (notDuplicates.Contains(s[i]))
+                while (tempString.Count > 0 && characters.Contains(s[i]))
                 {
-                    while (tempString.Count > 0 && notDuplicates.Contains(s[i]))
-                    {
-                        notDuplicates.Remove(tempString.Dequeue());
-                    }
+                    characters.Remove(tempString.Dequeue());
                 }
 
-                notDuplicates.Add(s[i]);
+                characters.Add(s[i]);
                 tempString.Enqueue(s[i]);
                 maxLength = tempString.Count > maxLength ? tempString.Count : maxLength;
             }
