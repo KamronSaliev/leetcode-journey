0ce8a73434e3184664c0b9dc9e49a9f3463447bc
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-03-06 23:11:10 +0500

    Added DFS and BFS solutions

--- leetcode/Easy/Easy111_MinimumDepthOfBinaryTree.cs
+++ leetcode/Easy/Easy111_MinimumDepthOfBinaryTree.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using LeetCode.Utilities;
 
 namespace LeetCode.Easy
@@ -9,6 +10,14 @@ namespace LeetCode.Easy
     public class Easy111_MinimumDepthOfBinaryTree
     {
         public int MinDepth(TreeNode root)
+        {
+            // solution by DFS approach
+            // return DFS(root);
+
+            return BFS(root);
+        }
+
+        private int DFS(TreeNode root)
         {
             if (root == null)
             {
@@ -22,8 +31,50 @@ namespace LeetCode.Easy
             {
                 return Math.Max(leftMinDepth, rightMinDepth) + 1;
             }
-            
+
             return Math.Min(leftMinDepth, rightMinDepth) + 1;
+        }
+
+        private int BFS(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            var depth = 0;
+
+            while (queue.Count != 0)
+            {
+                var size = queue.Count;
+
+                for (var i = 0; i < size; i++)
+                {
+                    var current = queue.Dequeue();
+
+                    if (current.left != null)
+                    {
+                        queue.Enqueue(current.left);
+                    }
+
+                    if (current.right != null)
+                    {
+                        queue.Enqueue(current.right);
+                    }
+
+                    if (current.left == null && current.right == null)
+                    {
+                        return depth + 1;
+                    }
+                }
+
+                depth++;
+            }
+
+            return 0;
         }
     }
 }
