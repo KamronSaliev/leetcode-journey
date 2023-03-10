1193a0e5c3b247f64ff06b92fcb5ce24856a3d2f
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-03-10 23:31:46 +0500

    Added link for 2396. Strictly Palindromic Number

--- leetcode/Medium/Medium2396_StrictlyPalindromicNumber.cs
+++ leetcode/Medium/Medium2396_StrictlyPalindromicNumber.cs
@@ -1,5 +1,8 @@
 namespace LeetCode.Medium
 {
+    /// <summary>
+    ///     https://leetcode.com/problems/strictly-palindromic-number/
+    /// </summary>
     public class Medium2396_StrictlyPalindromicNumber
     {
         public bool IsStrictlyPalindromic(int n)
