50165ced019d4c4c341482f1e94df073a30338f7
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-03-10 23:24:33 +0500

    Added solution for 2396. Strictly Palindromic Number

---
+++ leetcode/Medium/Medium2396_StrictlyPalindromicNumber.cs
@@ -0,0 +1,10 @@
+namespace LeetCode.Medium
+{
+    public class Medium2396_StrictlyPalindromicNumber
+    {
+        public bool IsStrictlyPalindromic(int n)
+        {
+            return false;
+        }
+    }
+}
