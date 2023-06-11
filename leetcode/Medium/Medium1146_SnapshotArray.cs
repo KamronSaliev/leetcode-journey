0d7f0ade9dfd972f7bd7501fcf717a82af1a65c5
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-06-11 18:46:35 +0500

    Added solution for 1146. Snapshot Array

---
+++ leetcode/Medium/Medium1146_SnapshotArray.cs
@@ -0,0 +1,34 @@
+namespace LeetCode.Medium
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/snapshot-array/
+    /// </summary>
+    public class Medium1146_SnapshotArray
+    {
+        /**
+         * Your SnapshotArray object will be instantiated and called as such:
+         * SnapshotArray obj = new SnapshotArray(length);
+         * obj.Set(index,val);
+         * int param_2 = obj.Snap();
+         * int param_3 = obj.Get(index,snap_id);
+         */
+        public class SnapshotArray
+        {
+            public SnapshotArray(int length)
+            {
+            }
+
+            public void Set(int index, int val)
+            {
+            }
+
+            public int Snap()
+            {
+            }
+
+            public int Get(int index, int snap_id)
+            {
+            }
+        }
+    }
+}
