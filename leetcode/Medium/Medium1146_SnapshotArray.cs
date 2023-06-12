1185fe6ec23c09ac7d89c7173904716d68f6b77d
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-06-12 12:07:15 +0500

    Added solution for 1146. Snapshot Array

--- leetcode/Medium/Medium1146_SnapshotArray.cs
+++ leetcode/Medium/Medium1146_SnapshotArray.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace LeetCode.Medium
 {
     /// <summary>
@@ -14,20 +17,61 @@ namespace LeetCode.Medium
          */
         public class SnapshotArray
         {
+            private readonly Dictionary<int, int>[] _snapshots;
+            private int _snapId;
+
             public SnapshotArray(int length)
             {
+                _snapshots = new Dictionary<int, int>[length];
+                _snapId = 0;
             }
 
             public void Set(int index, int val)
             {
+                _snapshots[index] ??= new Dictionary<int, int>();
+                _snapshots[index][_snapId] = val;
             }
 
             public int Snap()
             {
+                return _snapId++;
             }
 
             public int Get(int index, int snap_id)
             {
+                var result = _snapshots[index];
+
+                if (result == null)
+                {
+                    return 0;
+                }
+
+                if (result.TryGetValue(snap_id, out var val))
+                {
+                    return val;
+                }
+
+                var arr = result.Select(k => (k.Key, k.Value)).ToArray();
+                var left = 0;
+                var right = arr.Length - 1;
+                
+                while (left <= right)
+                {
+                    var mid = left + (right - left) / 2;
+                    var (snap, value) = arr[mid];
+                    
+                    if (snap < snap_id)
+                    {
+                        val = value;
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+
+                return val;
             }
         }
     }
