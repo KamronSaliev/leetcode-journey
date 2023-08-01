280741ccddc758d4fd11dd66ca41b5d30e8fe40c
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-08-01 16:29:04 +0500

    Added solution for 77. Combinations

--- leetcode/Medium/Medium77_Combinations.cs
+++ leetcode/Medium/Medium77_Combinations.cs
@@ -9,25 +9,25 @@ namespace LeetCode.Medium
     {
         public IList<IList<int>> Combine(int n, int k)
         {
-            var ans = new List<IList<int>>();
+            var result = new List<IList<int>>();
 
-            Backtrack(k, n, 1, new List<int>(), ans);
+            Backtrack(k, n, 1, new List<int>(), result);
 
-            return ans;
+            return result;
         }
 
-        private void Backtrack(int k, int n, int start, List<int> list, IList<IList<int>> ans)
+        private void Backtrack(int k, int n, int start, List<int> list, IList<IList<int>> result)
         {
             if (list.Count == k)
             {
-                ans.Add(new List<int>(list));
+                result.Add(new List<int>(list));
                 return;
             }
 
             for (var i = start; i <= n; i++)
             {
                 list.Add(i);
-                Backtrack(k, n, i + 1, list, ans);
+                Backtrack(k, n, i + 1, list, result);
                 list.RemoveAt(list.Count - 1);
             }
         }
