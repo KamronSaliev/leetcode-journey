e7e4bda8e56d845cc3edd1b4e72dfc9ae6834018
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-12-02 23:46:36 +0500

    Updated solution for 1662. Check If Two String Arrays are Equivalent

--- leetcode/Easy/Easy1662_CheckIfTwoStringArraysAreEquivalent.cs
+++ leetcode/Easy/Easy1662_CheckIfTwoStringArraysAreEquivalent.cs
@@ -7,20 +7,7 @@ namespace LeetCode.Easy
     {
         public bool ArrayStringsAreEqual(string[] word1, string[] word2)
         {
-            var s1 = string.Empty;
-            var s2 = string.Empty;
-
-            for (var i = 0; i < word1.Length; i++)
-            {
-                s1 += word1[i];
-            }
-
-            for (var i = 0; i < word1.Length; i++)
-            {
-                s2 += word2[i];
-            }
-
-            return s1 == s2;
+            return string.Concat(word1) == string.Concat(word2);
         }
     }
 }
