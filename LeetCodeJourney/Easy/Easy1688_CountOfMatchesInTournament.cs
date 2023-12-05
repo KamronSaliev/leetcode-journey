824bbdce70cf73c47587f9b44a68c8d6e7bfc7b4
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-12-05 14:19:14 +0500

    Added solution for 1688. Count of Matches in Tournament

---
+++ LeetCodeJourney/Easy/Easy1688_CountOfMatchesInTournament.cs
@@ -0,0 +1,13 @@
+namespace LeetCode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/count-of-matches-in-tournament/
+    /// </summary>
+    public class Easy1688_CountOfMatchesInTournament
+    {
+        public int NumberOfMatches(int n)
+        {
+            return n - 1;
+        }
+    }
+}
