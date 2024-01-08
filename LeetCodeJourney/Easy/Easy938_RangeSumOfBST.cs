0127b84e69c129cef9864d6c1a919a6d38493083
Komroniddin Soliev <kamron.saliev5@gmail.com>
2024-01-08 13:14:08 +0500

    Added solution for 938. Range Sum of BST

---
+++ LeetCodeJourney/Easy/Easy938_RangeSumOfBST.cs
@@ -0,0 +1,30 @@
+using LeetCode.Utilities;
+
+namespace LeetCode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/range-sum-of-bst/
+    /// </summary>
+    public class Easy938_RangeSumOfBST
+    {
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.val < low)
+            {
+                return RangeSumBST(root.right, low, high);
+            }
+
+            if (root.val > high)
+            {
+                return RangeSumBST(root.left, low, high);
+            }
+
+            return root.val + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        }
+    }
+}
