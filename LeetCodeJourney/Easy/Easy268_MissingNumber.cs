e56c2927446526a8c5d77e012f673cd31db05b23
Komroniddin Soliev <kamron.saliev5@gmail.com>
2024-02-20 22:39:38 +0500

    Simplified solution for 268. Missing Number

--- LeetCodeJourney/Easy/Easy268_MissingNumber.cs
+++ LeetCodeJourney/Easy/Easy268_MissingNumber.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 
 namespace LeetCode.Easy
 {
@@ -9,27 +9,7 @@ namespace LeetCode.Easy
     {
         public int MissingNumber(int[] nums)
         {
-            var occurrences = new Dictionary<int, int>();
-
-            for (int i = 0; i <= nums.Length; i++)
-            {
-                occurrences.Add(i, 0);
-            }
-            
-            for (var i = 0; i < nums.Length; i++)
-            {
-                occurrences[nums[i]]++;
-            }
-
-            foreach (var occurrence in occurrences)
-            {
-                if (occurrence.Value == 0)
-                {
-                    return occurrence.Key;
-                }
-            }
-
-            return -1;
+            return nums.Length * (nums.Length + 1) / 2 - nums.Sum();
         }
     }
 }
