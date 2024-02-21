c02f8b1ee73b2a677c83746a19edd3dcf54738bb
Komroniddin Soliev <kamron.saliev5@gmail.com>
2024-02-21 23:21:23 +0500

    Added solution for 201. Bitwise AND of Numbers Range

---
+++ LeetCodeJourney/Medium/Medium201_BitwiseANDOfNumbersRange.cs
@@ -0,0 +1,22 @@
+namespace LeetCode.Medium
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/bitwise-and-of-numbers-range/
+    /// </summary>
+    public class Medium201_BitwiseANDOfNumbersRange
+    {
+        public int RangeBitwiseAnd(int left, int right)
+        {
+            var count = 0;
+
+            while (left != right)
+            {
+                count++;
+                left >>= 1;
+                right >>= 1;
+            }
+
+            return left << count;
+        }
+    }
+}
