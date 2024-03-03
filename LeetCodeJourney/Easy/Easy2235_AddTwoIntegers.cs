ff88f26eb5163adca9295f7bfc28997b9ce18082
Komroniddin Soliev <kamron.saliev5@gmail.com>
2024-03-03 22:22:19 +0500

    Added solution for 2235. Add Two Integers

---
+++ LeetCodeJourney/Easy/Easy2235_AddTwoIntegers.cs
@@ -0,0 +1,13 @@
+namespace LeetCode.Easy
+{
+    /// <summary>
+    ///     https://leetcode.com/problems/add-two-integers
+    /// </summary>
+    public class Easy2235_AddTwoIntegers
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
